43e51a2bedc6b1e72114ed7ce8bff0eda1c70f20
Feather Knee <featherknee@gmail.com>
2018-01-03 17:13:07 -0800

    add interactively selected wizard

--- Wizards.js
+++ Wizards.js
@@ -1,20 +1,38 @@
-import React, { Component } from 'react'
-import wizardStore from './wizardStore'
+import React, { Component } from "react";
+import wizardStore from "./wizardStore";
 
 class Wizards extends Component {
   state = {
-    items: wizardStore
+    wizards: wizardStore,
+    selectedWizard: "Harry"
+  };
+
+  handleWizardChange(e) {
+    e.preventDefault();
+    this.setState({ selectedWizard: e.target.dataset.name });
   }
 
   render() {
-    const { items } = this.state;
+    const { wizards, selectedWizard } = this.state;
     return (
-      <div className='container'>
-      { items.map(item => (
-        <div key={item.id} className={`wizard ${item.house}`}>{item.name} from House {item.house}</div>
-      ))}
+      <div className="container">
+        <div className="selected-wizard">
+          The selected wizard is <i>{selectedWizard}</i>
+        </div>
+        <div className="wizards-container">
+          {wizards.map(wiz =>
+            <div
+              key={wiz.id}
+              data-name={wiz.name}
+              onClick={e => this.handleWizardChange(e)}
+              className={`wizard ${wiz.house}`}
+            >
+              {wiz.name} from House {wiz.house}
+            </div>
+          )}
+        </div>
       </div>
-    )
+    );
   }
 }
-export default Wizards
+export default Wizards;
